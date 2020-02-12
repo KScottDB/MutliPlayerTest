d4f2df3b23187e595a41eb676b8c32bcba3363c5
KScott <kscott.db@gmail.com>
2020-02-12 14:41:44 -0800

    e

---
+++ MultiPlayerTest/FMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiPlayerTest
+{
+    public partial class FMain : Form
+    {
+        public enum States
+        {
+            SearchingForServers,
+            Client,
+            Server
+        }
+
+        private States State;
+
+        public FMain(States state)
+        {
+            InitializeComponent();
+            State = state;
+
+        }
+    }
+}
---
+++ MultiPlayerTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiPlayerTest
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            char choice = ' ';
+            
+            Console.Write("[C]lient or [S]erver? ");
+
+            // C or S
+            while ("CScs".Contains(choice = Console.ReadKey().KeyChar)) { }
+
+            Console.Write("\n");
+
+            // Turn into FMain.States to pass into FMain
+            FMain.States enuch;
+            switch (choice)
+            {
+                case 'C':
+                case 'c':
+                    enuch = FMain.States.SearchingForServers;
+                    break;
+
+                case 'S':
+                case 's':
+                    enuch = FMain.States.Server;
+                    break;
+
+                default:
+                    enuch = FMain.States.Server;
+                    break;
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FMain(enuch));
+        }
+    }
+}
